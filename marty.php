1532a36fd5a580a39c75cbca364b379733e1bdf0
Rochelle Lewis <rlewis37@gmail.com>
2017-08-09 17:02:02 -0600

    checkpoint commit - marty solution

---
+++ marty.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- LOAD ALL DEPENDENCY LINKS FROM FILE -->
+		<?php require_once("lib/head-utils.php");?>
+
+		<!-- JavaScript - Marty Level JS-->
+		<script src="js/marty.js"></script>
+
+		<title>JavaScript Assignment | Marty Level</title>
+	</head>
+	<body>
+
+		<!-- LOAD NAVBAR FROM FILE -->
+		<?php require_once("lib/navbar.php"); ?>
+
+		<div class="container">
+			<h2>My Calculator From Hell</h2>
+			<div class="row">
+				<div class="col-sm-2">
+					<label for="num1">Number #1</label>
+					<input type="number" id="num1" class="form-control">
+				</div>
+				<div class="col-sm-2">
+					<label for="num2">Number #2</label>
+					<input type="number" id="num2" class="form-control">
+				</div>
+
+			</div>
+		</div>
+
+	</body>
+</html>
