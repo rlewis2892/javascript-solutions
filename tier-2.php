da3aebb96dcda8b3298ebd9440dcc1cd3d0c9f47
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2020-05-12 02:44:56 -0600

    update tier II

--- tier-2.php
+++ tier-2.php
@@ -3,50 +3,54 @@
 	<head>
 		<!-- LOAD ALL DEPENDENCY LINKS FROM FILE -->
 		<?php require_once("lib/head-utils.php");?>
-
-		<!-- JavaScript - Required Tier-->
-		<script src="js/optional.js"></script>
-
-		<title>JavaScript Assignment | Optional Tier</title>
+		<title>Literary Mutilator | Tier II</title>
 	</head>
 	<body>
 
 		<!-- LOAD NAVBAR FROM FILE -->
 		<?php require_once("lib/navbar.php"); ?>
 
-		<div class="container">
+		<main>
+			<div class="container">
 
-			<!-- find/replace -->
-			<section>
-				<div id="find-replace">
-					<h2>Find &amp; Replace =^. .^=</h2>
-					<p>Human give me attention meow hide from vacuum cleaner human give me attention meow. Bathe private parts with tongue then lick owner's face walk on car leaving trail of paw prints on hood and windshield. Stare at the wall, play with food and get confused by dust soft kitty warm kitty little ball of furr, and eat owner's food loves cheeseburgers mew chase the pig around the house. Attack dog, run away and pretend to be victim hunt by meowing loudly at 5am next to human slave food dispenser but jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed who's the baby, yet jump around on couch, meow constantly until given food. Soft kitty warm kitty little ball of furr missing until dinner time always hungry. Chase after silly colored fish toys around the house. Shake treat bag.</p>
-				</div><!-- /#find-replace -->
+				<h1 class="pacifico--font">Tier II</h1>
+				<hr class="mt-5 bg-secondary">
 
-				<div class="row">
-					<div class="col-sm-6">
-						<div class="form-group">
-							<label for="txt-find">Find</label>
-							<input type="text" class="form-control" id="txt-find" maxlength="25">
+				<!-- find/replace -->
+				<section>
+					<div id="find-replace">
+						<h2>Find &amp; Replace =^. .^=</h2>
+						<p>Human give me attention meow hide from vacuum cleaner human give me attention meow. Bathe private parts with tongue then lick owner's face walk on car leaving trail of paw prints on hood and windshield. Stare at the wall, play with food and get confused by dust soft kitty warm kitty little ball of furr, and eat owner's food loves cheeseburgers mew chase the pig around the house. Attack dog, run away and pretend to be victim hunt by meowing loudly at 5am next to human slave food dispenser but jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed who's the baby, yet jump around on couch, meow constantly until given food. Soft kitty warm kitty little ball of furr missing until dinner time always hungry. Chase after silly colored fish toys around the house. Shake treat bag.</p>
+					</div><!-- /#find-replace -->
+
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="form-group">
+								<label for="txt-find">Find</label>
+								<input type="text" class="form-control" id="txt-find" maxlength="25">
+							</div>
+							<div class="form-group">
+								<label for="txt-replace">Replace</label>
+								<input type="text" class="form-control" id="txt-replace" maxlength="25">
+							</div>
+							<button type="button" class="btn btn-primary" onclick="findReplace();">Go!</button>
+
 						</div>
-						<div class="form-group">
-							<label for="txt-replace">Replace</label>
-							<input type="text" class="form-control" id="txt-replace" maxlength="25">
-						</div>
-						<button type="button" class="btn btn-primary" onclick="findReplace();">Go!</button>
-
 					</div>
-				</div>
-			</section>
+				</section>
 
-			<!-- random shuffle -->
-			<section>
-				<div class="text-center">
-					<h1>Corporate Shuffle</h1>
-					<button id="btn-shuffle" class="btn btn-danger btn-lg m-t-1" onclick="shufflize();">(ノ°▽°)ノ︵┻━┻</button>
-				</div>
-				<p id="shuffle" class="well lead m-t-1">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment. Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring. Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring. Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line. Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions without functional solutions. Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
-			</section>
-		</div>
+				<!-- random shuffle -->
+				<section>
+					<div class="text-center">
+						<h1>Corporate Shuffle</h1>
+						<button id="btn-shuffle" class="btn btn-danger btn-lg m-t-1" onclick="shufflize();">(ノ°▽°)ノ︵┻━┻</button>
+					</div>
+					<p id="shuffle" class="well lead m-t-1">Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment. Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring. Capitalise on low hanging fruit to identify a ballpark value added activity to beta test. Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring. Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line. Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration. Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI. Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas. Dramatically maintain clicks-and-mortar solutions without functional solutions. Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas. Dynamically innovate resource-leveling customer service for state of the art customer service.</p>
+				</section>
+			</div>
+		</main>
+
+		<!-- JavaScript - Tier II -->
+		<script src="js/tier-2.js"></script>
 	</body>
 </html>
